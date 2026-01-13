f11db1919c1ad7669777d7b712f6cfbc0f2cde18
SpaceCatH <jonathan.e.eng@gmail.com>
2026-01-13 16:05:13 -0600

    Update main.py

--- main.py
+++ main.py
@@ -359,7 +359,3 @@ def get_strategy(
 # For local / Replit run
 # -----------------------------
 
-if __name__ == "__main__":
-    import uvicorn
-
-    uvicorn.run("backend.main:app", host="0.0.0.0", port=8080, reload=True)
