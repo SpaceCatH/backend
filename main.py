8d6f2a0e9a49e87738b3f600ed0aa20e1a442c9b
SpaceCatH <jonathan.e.eng@gmail.com>
2026-01-13 16:02:13 -0600

    Update main.py

--- main.py
+++ main.py
@@ -362,4 +362,4 @@ def get_strategy(
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run("backend.main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("backend.main:app", host="0.0.0.0", port=8080, reload=True)
